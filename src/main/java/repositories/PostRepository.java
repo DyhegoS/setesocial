c6ebd0c91bb580e2985d3977ad057104162f083a
DyhegoS <dyhegodasilva@gmail.com>
2025-06-30 18:25:37 -0300

    criado repositories

---
+++ src/main/java/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import entities.Post;
+
+public interface PostRepository extends JpaRepository<Post, Long>{
+
+}
