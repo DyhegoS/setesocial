5304c4e8222187df92a69f89c3cb25e9f11a08cd
DyhegoS <dyhegodasilva@gmail.com>
2025-07-01 17:21:15 -0300

    Funcionalidade de criação de usuário

---
+++ src/main/java/dev/seteoliveiras/setesocial/controller/UserController.java
@@ -0,0 +1,52 @@
+package dev.seteoliveiras.setesocial.controller;
+
+import java.util.Set;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import dev.seteoliveiras.setesocial.controller.dto.CreateUserDto;
+import dev.seteoliveiras.setesocial.entities.Role;
+import dev.seteoliveiras.setesocial.entities.User;
+import dev.seteoliveiras.setesocial.repositories.RoleRepository;
+import dev.seteoliveiras.setesocial.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+
+@RestController
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userRepository, RoleRepository roleRepository,
+            BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    @PostMapping("/users")
+    public ResponseEntity<Void> newUser(@RequestBody CreateUserDto dto){
+
+        var basicRole = roleRepository.findByName(Role.Values.USER.name());
+        var userFromDb = userRepository.findByUsername(dto.username());
+        if(userFromDb.isPresent()){
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        var user = new User();
+        user.setUsername(dto.username());
+        user.setPassword(passwordEncoder.encode(dto.password()));
+        user.setRoles(Set.of(basicRole));
+
+        userRepository.save(user);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/dev/seteoliveiras/setesocial/controller/dto/CreateUserDto.java
@@ -0,0 +1,5 @@
+package dev.seteoliveiras.setesocial.controller.dto;
+
+public record CreateUserDto(String username, String password) {
+
+}
