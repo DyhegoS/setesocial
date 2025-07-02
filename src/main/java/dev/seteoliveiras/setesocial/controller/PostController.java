9caf376a592c77e56567aff13a09dd0a575db4aa
DyhegoS <dyhegodasilva@gmail.com>
2025-07-02 10:31:48 -0300

    delete de posts

--- src/main/java/dev/seteoliveiras/setesocial/controller/PostController.java
+++ src/main/java/dev/seteoliveiras/setesocial/controller/PostController.java
@@ -2,14 +2,19 @@ package dev.seteoliveiras.setesocial.controller;
 
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import dev.seteoliveiras.setesocial.controller.dto.CreatePostDto;
 import dev.seteoliveiras.setesocial.entities.Post;
+import dev.seteoliveiras.setesocial.entities.Role;
 import dev.seteoliveiras.setesocial.repositories.PostRepository;
 import dev.seteoliveiras.setesocial.repositories.UserRepository;
 
@@ -34,6 +39,27 @@ public class PostController {
 
         postRepository.save(post);
 
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable("id") Long postId, JwtAuthenticationToken token){
+
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+
+        var post = postRepository.findById(postId)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        
+        var isAdmin = user.get().getRoles()
+                .stream().anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));            
+
+        if(isAdmin || post.getUser().getUserId().equals(UUID.fromString(token.getName()))){
+            postRepository.deleteById(postId);
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+        
         return ResponseEntity.ok().build();
     }
 }
